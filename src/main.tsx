8f0ff09b801520023ac6f61f9fee174942b1c2b7
Kiran Namawar <kirannamawar@gmail.com>
2025-12-03 16:58:23 +0530

    feat: integrate Redux Provider and Router in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router";
-import router from "./router.ts";
+import { Provider } from "react-redux";
+import store from "./redux/store.ts";
+import router from "./router.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </StrictMode>,
 );
